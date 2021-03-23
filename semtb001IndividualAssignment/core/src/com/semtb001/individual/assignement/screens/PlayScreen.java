abcc886757000022dcd67ce91d893b8d15584727
tully-xps <none_lol>
2021-03-23 19:00:08 +1030

    setup

---
+++ semtb001IndividualAssignment/core/src/com/semtb001/individual/assignement/screens/PlayScreen.java
@@ -0,0 +1,46 @@
+package com.semtb001.individual.assignement.screens;
+
+import com.badlogic.gdx.Screen;
+import com.semtb001.individual.assignement.semtb001IndividualAssignment;
+
+public class PlayScreen implements Screen {
+    
+    public PlayScreen(semtb001IndividualAssignment semtb001IndividualAssignment) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}

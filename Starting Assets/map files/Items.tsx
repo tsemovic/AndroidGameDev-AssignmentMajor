aaaae64a1e8cd038d5e7655981c82be71aeab2ba
tully-xps <none_lol>
2021-04-11 10:50:02 +0930

    Removed Unused Assets

--- Starting Assets/map files/Items.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Items" tilewidth="70" tileheight="70" tilecount="64" columns="8">
- <image source="items_spritesheet.png" width="576" height="576"/>
-</tileset>
--- Starting Assets/map files/mymap/tiles_spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tiles_spritesheet" tilewidth="70" tileheight="70" tilecount="169" columns="13">
- <image source="../../../semtb001IndividualAssignment/android/assets/mapFiles/tiles_spritesheet.png" width="914" height="936"/>
-</tileset>
